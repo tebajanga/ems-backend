b7c9b49381a383bcdf1b6defa9fdf8ba4d8de386
Timothy <anthony.timothy90@gmail.com>
2025-07-01 18:34:36 +0300

    feat(Employee) Adding endpoint to return all employees

--- src/main/java/com/codekutu/ems/service/EmployeeService.java
+++ src/main/java/com/codekutu/ems/service/EmployeeService.java
@@ -1,9 +1,14 @@
 package com.codekutu.ems.service;
 
 import com.codekutu.ems.dto.EmployeeDto;
+import com.codekutu.ems.entity.Employee;
+
+import java.util.List;
 
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
 
     EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployees();
 }
